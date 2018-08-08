b43db210554ddc523be4e466603a013f409eddd3
Tyler Toth <tyler@cloud-elements.com>
2018-08-08 16:51:15 -0500

    :sunglasses: Deployed serverless to zeit.now and mlab

--- configs.js
+++ configs.js
@@ -1,5 +1,7 @@
 module.exports = {
-  "mongoUrl": "mongodb://localhost:27017/",
-  "dbName": "neverland",
+  // "mongoUrl": "mongodb://localhost:27017/",
+  "mongoUrl": "mongodb://system:system1@ds115762.mlab.com:15762/",
+  // "dbName": "neverland"
+  "dbName": "specio",
   "port": 3000
 }
