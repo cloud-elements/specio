1753aa99ddcb23c5b50ffcbedd22189c9f04b437
Tyler Toth <tyler@cloud-elements.com>
2019-05-01 17:43:20 -0500

    Changd mlab db as the old one went kaput

--- configs.js
+++ configs.js
@@ -1,8 +1,10 @@
 module.exports = {
   // "mongoUrl": "mongodb://localhost:27017/",
-  "mongoUrl": "mongodb://system:system1@ds115762.mlab.com:15762/",
+  // "mongoUrl": "mongodb://system:system1@ds115762.mlab.com:15762/",
+  "mongoUrl": "mongodb://system:system1@ds147436.mlab.com:47436/",
   // "dbName": "neverland"
-  "dbName": "specio",
+  // "dbName": "specio",
+  "dbName": "specio-eb-ub",
   "port": 3000,
   "logDirectory": "logs",
   "logFile": "buckets.log"
